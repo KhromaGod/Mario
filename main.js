aa86e92c6ed4eedb32645a386aef08aba454bcdf
KhromaGod <82552599+KhromaGod@users.noreply.github.com>
2022-11-24 10:25:13 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -9,11 +9,30 @@ function setup() {
 	canvas.parent('canvas');
 	
 	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses)
 }
 
 function draw() {
 	game()
 } 
+function modelLoaded(){
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results){
+	if(results.length>0){
+		console.log(results);
+		noseX= results[0].pose.nose.x;
+		noseY= results[0].pose.nose.y;
+		console.log("noseX=" + noseX+", NoseY"+ noseY);
+	}
+}
 
 
 
